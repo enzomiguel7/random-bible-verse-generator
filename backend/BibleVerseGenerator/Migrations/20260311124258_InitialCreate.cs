dee9643ed27b590972399e03047ddf7c6d26e8e4
enzo <enzooliveirasilva90@gmail.com>
2026-03-11 10:38:29 -0300

    adicioando migrations

---
+++ backend/BibleVerseGenerator/Migrations/20260311124258_InitialCreate.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibleVerseGenerator.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Verses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Book = table.Column<string>(type: "TEXT", nullable: false),
+                    Chapter = table.Column<byte>(type: "INTEGER", nullable: false),
+                    Verse = table.Column<string>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Verses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagsVerses",
+                columns: table => new
+                {
+                    TagsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    VersesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagsVerses", x => new { x.TagsId, x.VersesId });
+                    table.ForeignKey(
+                        name: "FK_TagsVerses_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagsVerses_Verses_VersesId",
+                        column: x => x.VersesId,
+                        principalTable: "Verses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagsVerses_VersesId",
+                table: "TagsVerses",
+                column: "VersesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagsVerses");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Verses");
+        }
+    }
+}
--- backend/BibleVerseGenerator/Program.cs
+++ backend/BibleVerseGenerator/Program.cs
@@ -8,3 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BibleContext>(opt => opt.UseSqlite("Data Source=Bible.db"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
+
+app.MapGet("/verses", async (BibleContext bc) => 
+   await bc.Verses.ToListAsync());
+
+app.Run();
