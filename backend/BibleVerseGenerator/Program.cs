0c8b37e8c10d9508bea76752abe2b4d1f1757836
enzo <enzooliveirasilva90@gmail.com>
2026-03-12 22:41:58 -0300

    data seeding complete

--- backend/BibleVerseGenerator/Program.cs
+++ backend/BibleVerseGenerator/Program.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using BibleVerseGenerator.Data;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -8,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BibleContext>(opt => opt.UseSqlite("Data Source=Bible.db"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
+
+// adiciona um scope para conseguir ter acesso ao BibleContext que é scoped por padrão, só cria uma instância em requisições HTTP
+using (var scope = app.Services.CreateScope())
+{
+   var context = scope.ServiceProvider.GetRequiredService<BibleContext>();
+   DbInitializer.Seed(context);
+   context.SaveChanges();
+}
 
 app.MapGet("/verses", async (BibleContext bc) => 
    await bc.Verses.ToListAsync());
